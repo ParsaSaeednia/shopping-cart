3681a278f21ad6b701f740232aa51f177ce8de1f
ParsaSaeednia <p.parsa31@gmail.com>
2024-02-03 15:27:19 +0330

    init

---
+++ src/components/AppFooter.vue
@@ -0,0 +1,5 @@
+<template>
+  <footer class="text-muted py-5 bg-danger">
+    <p class="text-white text-center">Just a Footer in case needed</p>
+  </footer>
+</template>
---
+++ src/components/AppHeader.vue
@@ -0,0 +1,31 @@
+<template>
+  <header>
+    <div class="collapse bg-dark" id="navbarHeader">
+      <div class="container">
+        <div class="row">
+
+        </div>
+      </div>
+    </div>
+    <div class="navbar navbar-dark bg-danger shadow-sm">
+      <div class="container">
+        <router-link :to="{ name: 'Home' }" class="navbar-brand d-flex align-items-center">
+          <strong>SHOPPING CART</strong>
+        </router-link>
+        <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
+          <li class="nav-item me-3">
+            <router-link class="nav-link" :class="$route.name == 'Cart' ? 'active' : ''" aria-current="page"
+              :to="{ name: 'Cart' }">
+              <i class="bi bi-cart3 h3"></i>
+              <span v-if="$store.state.cart.length > 0"
+                class="align-items-center justify-content-center translate-middle badge rounded-pill bg-secondary">
+                {{ $store.state.cart.length }}
+              </span>
+            </router-link>
+          </li>
+        </ul>
+
+      </div>
+    </div>
+  </header>
+</template>
