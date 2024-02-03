e656a1dfde6ce178ce530150503fde3a219b4173
Parsa <66898092+ParsaSaeednia@users.noreply.github.com>
2024-02-03 15:32:27 +0330

    Update main.js

--- src/main.js
+++ src/main.js
@@ -6,6 +6,4 @@ import "bootstrap"
 import "bootstrap-icons/font/bootstrap-icons.css"
 import router from "./router"
 import store from "./store"
-import { createHead } from '@vueuse/head'
-const head = createHead()
-createApp(App).use(head).use(router).use(store).mount('#app')
+createApp(App).use(router).use(store).mount('#app')
